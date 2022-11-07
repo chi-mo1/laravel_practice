1f6d5e292a54c501287ca73a7c191bbe55906200
github chi-mo1 <whats.radwimps.seamo.1@gamil.com>
2022-11-08 07:10:42 +0900

    マイグレーション・シーディングについて。６章Eloquentの基本Personについて。

--- database/seeders/PeopleTableSeeder.php
+++ database/seeders/PeopleTableSeeder.php
@@ -13,29 +13,30 @@ class PeopleTableSeeder extends Seeder
      * @return void
      */
     public function run()
-{
     {
-        $param = [
-            'name' => 'taro',
-            'mail' => 'taro@yamada.jp',
-            'age' => '12',
-        ];
-        DB::table('people')->insert($param);
-    }
-    {
-        $param = [
-            'name' => 'hanako',
-            'mail' => 'hanako@flowaer.jp',
-            'age' => '34',
-        ];
-        DB::table('people')->insert($param);
-    }
-    {
-        $param = [
-            'name' => 'sachiko',
-            'mail' => 'sachiko@happy.jp',
-            'age' => '56',
-        ];
-        DB::table('people')->insert($param);
+        {
+            $param = [
+                'name' => 'taro',
+                'mail' => 'taro@yamada.jp',
+                'age' => '12',
+            ];
+            DB::table('people')->insert($param);
+        }
+        {
+            $param = [
+                'name' => 'hanako',
+                'mail' => 'hanako@flower.jp',
+                'age' => '34',
+            ];
+            DB::table('people')->insert($param);
+        }
+        {
+            $param = [
+                'name' => 'sachiko',
+                'mail' => 'sachiko@happy.jp',
+                'age' => '56',
+            ];
+            DB::table('people')->insert($param);
+        }
     }
 }
